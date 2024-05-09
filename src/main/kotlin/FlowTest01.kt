faa6f7c06788d2a1c4eb3299dec7958ed3832418
NikolayTkachenko <tkachenni@mail.ru>
2024-05-09 13:39:54 +0300

    Update logic sample

--- src/main/kotlin/FlowTest01.kt
+++ src/main/kotlin/FlowTest01.kt
@@ -1,11 +1,7 @@
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 import java.math.BigInteger
+import kotlin.coroutines.EmptyCoroutineContext
 
 object FlowTest01 {
 
@@ -14,10 +10,138 @@ object FlowTest01 {
         //test01()
         //test02()
 
+        runBlocking {
+            //test03()
+        }
+        //test04()
+        test05()
+
+    }
+    private fun test05() {
+        val flow01 = flow {
+            delay(100)
+
+            println("Emiiting first value")
+            emit(1)
+
+            delay(100)
+
+            println("Emitting second value")
+            emit(2)
+        }
+
+        val scope = CoroutineScope(EmptyCoroutineContext)
+        flow01
+            .onEach { println("Received $it with launchIn()") }
+            .launchIn(scope)
+
+
+        scope.launch {
+            flow01.collect {
+                println("Received $it in collect")
+            }
+        }
+
+        Thread.sleep(2000)
+
     }
 
-    private fun test03() {
-        
+    private fun test04() {
+        val flow01 = flow {
+            delay(100)
+
+            println("Emiiting first value")
+            emit(1)
+
+            delay(100)
+
+            println("Emitting second value")
+            emit(2)
+        }
+
+        val flow02 = flow {
+            delay(100)
+
+            println("Emiiting first value")
+            emit(1)
+
+        }
+
+        runBlocking {
+            flow01.collect {receivedValue ->
+                println("Received value $receivedValue")
+
+            }
+        }
+
+        runBlocking {
+            val item = flow01.first()
+            println("Received $item")
+        }
+
+        runBlocking {
+            val item = flow01.first{ it > 1}
+            println("Received $item")
+        }
+
+        runBlocking {
+            val item = flow01.last()
+            println("Received $item")
+        }
+
+        runBlocking {
+            val item = flow02.single() //должно быть тольуо одно
+            println("Received $item")
+        }
+
+        runBlocking {
+            val item = flow01.toSet()
+            println("Received set $item")
+        }
+
+        runBlocking {
+            val item = flow01.toList()
+            println("Received list $item")
+        }
+
+        runBlocking {
+            val item = flow01.fold(6) { accumulator, emittedItem ->
+                accumulator + emittedItem
+            }
+            println("Received value =  $item")
+        }
+
+    }
+
+    private suspend fun test03() {
+        val firstFlow = flowOf(1).collect{ emittedValue ->
+            println("firstFlow: $emittedValue")
+        }
+        val secondFlow = flowOf(1, 2, 3)
+
+        secondFlow .collect{ emittedValue ->
+            println("secondFlow: $emittedValue")
+        }
+        val thirdFlow = flowOf<Any>(1, "two", 3)
+        thirdFlow.collect{ emittedValue ->
+            println("thirdFlow: $emittedValue")
+        }
+
+        listOf("A", "B", "C").asFlow().collect{ emittedValue ->
+            println("asFlow: $emittedValue")
+        }
+
+        flow {
+            delay(200)
+            emit("item emitted after 2000ms")
+
+            secondFlow.collect { emittedValue ->
+                emit(emittedValue)
+            }
+            emitAll(thirdFlow)
+        }.collect{ emittedValue ->
+            println("flow{}: $emittedValue")
+        }
     }
 
     private fun test02() = runBlocking {
